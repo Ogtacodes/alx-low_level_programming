121c204e2a07ce6ba9841addfdbac3cf85ad5d64
Ogtacodes <ogtadesigns@gmail.com>
2023-08-14 12:38:54 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Begining
+ * Return: Definitely 0 (success)
+ */
+int main(void)
+{
+	int n = 97;
+	int o = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+	}
+	while (o <= 90)
+	{
+		putchar(o);
+	}
+	putchar('\n');
+	return (0);
+}
