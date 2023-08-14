1a60748a24e1288f0e92bf3de0bbe0f8e6ab5b8e
Ogtacodes <ogtadesigns@gmail.com>
2023-08-14 14:18:34 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - begining
+ * Return: Definetly 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int o;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (o = 65; o <= 70; o++)
+	{
+		putchar(o);
+	}
+	putchar('\n');
+	return (0);
+}
