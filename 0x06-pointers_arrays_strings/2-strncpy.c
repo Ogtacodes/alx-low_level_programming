de0499e75a4016c0545de6a543b1a45ada25b1b7
Ogtacodes <ogtadesigns@gmail.com>
2023-08-24 20:03:54 -0700

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy -  copies a string.
+ * @dest: value one
+ * @src: value two
+ * @n: input value
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int k;
+
+	k = 0;
+	while (k < n && src[k] != '\0')
+	{
+		dest[k] = src[k];
+		k++;
+	}
+	return (dest);
+}
