3a9336957455a6360c8ae2bc42641023fd4afb6f
Ogtacodes <ogtadesigns@gmail.com>
2023-09-14 14:14:44 -0700

    SUM

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,8 +5,8 @@
  * sum_them_all - returns the sum of all its parameters
  * @n: the number of parameter to be entered
  * ...: variable number of parameter to calculate the sum
- * Return: If n == 0-0
- 	otherwise - the sum of all parameters
+ * Return: If n == 0 - 0
+ *	otherwise - the sum of all parameters
 */
 
 int sum_them_all(const unsigned int n, ...)
@@ -14,11 +14,11 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ak;
 	unsigned int i, sum;
 
-	va_start (ak, n);
+	va_start(ak, n);
 	sum = 0;
 	for (i = 0; i < n; i++)
-		sum+= va_arg (ak, int);
-	va_end (ak);
-	return sum;
+		sum += va_arg(ak, int);
+	va_end(ak);
+	return (sum);
 }
 
